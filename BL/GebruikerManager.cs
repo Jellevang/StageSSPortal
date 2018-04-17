bc7b3f71d52f645282ba32631684b5dd3b395dc6
samheirstrate <sam.heirstrate@student.kdg.be>
2018-04-17 12:04:46 +0200

    install identity

---
+++ BL/GebruikerManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security.DataProtection;
+
+namespace BL
+{
+    class GebruikerManager
+    {
+    }
+}
