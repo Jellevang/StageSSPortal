55c74c723b83ecc85e4dfa7ffe6436e7525b7507
samheirstrate <sam.heirstrate@student.kdg.be>
2018-05-11 15:29:29 +0200

    klantenaccount

---
+++ StageSSPortal/Controllers/api/KlantAccountController.cs
@@ -0,0 +1,26 @@
+﻿using BL;
+using Domain;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace StageSSPortal.Controllers.api
+{
+    public class KlantAccountController : ApiController
+    {
+        private readonly IKlantManager mgr = new KlantManager();
+        // GET: KlantAccount
+        [HttpGet]
+        [Route("api/Klant/KlantAccounts")]
+        [Authorize(Roles = "Klant")]
+        public IHttpActionResult GetKlantAccounts()
+        {
+            Klant k = mgr.GetKlant(User.Identity.GetUserName());
+            IEnumerable<Klant> Klanten = mgr.GetKlantenAccounts(k);
+            return Ok(Klanten);
+        }
+    }
+}
