8c22fe884d82913a990f0cf2d875d94f68265db8
Unknown <jellevang@hotmail.com>
2018-05-17 13:05:02 +0200

    fix

--- StageSSPortal/Controllers/KlantController.cs
+++ StageSSPortal/Controllers/KlantController.cs
@@ -106,7 +106,7 @@ namespace StageSSPortal.Controllers
                 return View("Create");
             }
             Klant naam = mgr.GetKlantByName(Klant.Naam);
-            if ((naam != null && naam.IsKlantAccount == false)
+            if (naam != null && naam.IsKlantAccount == false)
             {
                 ModelState.AddModelError("", "er is al reeds een klant met deze naam enkel accounts mogen dezelfde naam hebben");
                 return View("Create");
