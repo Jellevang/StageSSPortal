16eb30162b26aa3567fee30d39e678014d970e80
samheirstrate <sam.heirstrate@student.kdg.be>
2018-05-29 13:44:27 +0200

    paswoordcheck werkend

---
+++ StageSSPortal/Scripts/checkPassword.js
@@ -0,0 +1,72 @@
+﻿//$(document).ready(function () {
+//    checkPassWord();
+//});
+
+function checkPassword() {
+   
+        
+        var ucase = new RegExp("[A-Z]+");
+        var lcase = new RegExp("[a-z]+");
+        var num = new RegExp("[0-9]+");
+        var sign = new RegExp("[^A-Za-z0-9]");
+
+        if ($("#NewPassword").val().length >= 8) {
+            $("#8char").removeClass("glyphicon-remove");
+            $("#8char").addClass("glyphicon-ok");
+            $("#8char").css("color", "#00A41E");
+        } else {
+            $("#8char").removeClass("glyphicon-ok");
+            $("#8char").addClass("glyphicon-remove");
+            $("#8char").css("color", "#FF0004");
+        }
+        if (sign.test($("#NewPassword").val())) {
+            $("#sign").removeClass("glyphicon-remove");
+            $("#sign").addClass("glyphicon-ok");
+            $("#sign").css("color", "#00A41E");
+        } else {
+            $("#sign").removeClass("glyphicon-ok");
+            $("#sign").addClass("glyphicon-remove");
+            $("#sign").css("color", "#FF0004");
+        }
+
+        if (ucase.test($("#NewPassword").val())) {
+            $("#ucase").removeClass("glyphicon-remove");
+            $("#ucase").addClass("glyphicon-ok");
+            $("#ucase").css("color", "#00A41E");
+        } else {
+            $("#ucase").removeClass("glyphicon-ok");
+            $("#ucase").addClass("glyphicon-remove");
+            $("#ucase").css("color", "#FF0004");
+        }
+
+        if (lcase.test($("#NewPassword").val())) {
+            $("#lcase").removeClass("glyphicon-remove");
+            $("#lcase").addClass("glyphicon-ok");
+            $("#lcase").css("color", "#00A41E");
+        } else {
+            $("#lcase").removeClass("glyphicon-ok");
+            $("#lcase").addClass("glyphicon-remove");
+            $("#lcase").css("color", "#FF0004");
+        }
+
+        if (num.test($("#NewPassword").val())) {
+            $("#num").removeClass("glyphicon-remove");
+            $("#num").addClass("glyphicon-ok");
+            $("#num").css("color", "#00A41E");
+        } else {
+            $("#num").removeClass("glyphicon-ok");
+            $("#num").addClass("glyphicon-remove");
+            $("#num").css("color", "#FF0004");
+        }
+
+    if ($("#NewPassword").val() == $("#ConfirmPassword").val() && $("#NewPassword").val() != "") {
+            $("#pwmatch").removeClass("glyphicon-remove");
+            $("#pwmatch").addClass("glyphicon-ok");
+            $("#pwmatch").css("color", "#00A41E");
+        } else {
+            $("#pwmatch").removeClass("glyphicon-ok");
+            $("#pwmatch").addClass("glyphicon-remove");
+            $("#pwmatch").css("color", "#FF0004");
+        }
+    
+};
